e35f0ef88dad4cbc4d74bb6aee0f73f55b9f5993
mohit_sh <mohit_sh@hcl.com>
2018-10-02 15:40:15 +0530

    Initial

---
+++ spring-boot-actuators/src/main/java/com/actuators/ActuatorsApplication.java
@@ -0,0 +1,12 @@
+package com.actuators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorsApplication.class, args);
+	}
+}
